2005cfa56ef96d99b60a117a95daa577e60cd89f
刘文东 <18041196499@163.com>
2020-03-04 17:28:42 +0800

    比对 AdminOrderServiceImpl.java

--- src/service/admin/impl/AdminOrderServiceImpl.java
+++ src/service/admin/impl/AdminOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package service.admin.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,15 +19,34 @@ public class AdminOrderServiceImpl implements AdminOrderService{
 	@Autowired
 	private AdminOrderDao adminOrderDao;
 	
-	public String orderInfo(Model model) {
+	public String orderInfo(Model model,Integer pageNo) {
 		List<Map<String,Object>> list = adminOrderDao.orderInfo();
-		model.addAttribute("orderList", list);
+		int temp = list.size();
+		model.addAttribute("totalCount", temp);
+		int totalPage = 0;
+		if (temp == 0) {
+			totalPage = 0;//总页数
+		} else {
+			//返回大于或者等于指定表达式的最小整数
+			totalPage = (int) Math.ceil((double) temp / 10);
+		}
+		if (pageNo == null) {
+			pageNo = 1;
+		}
+		//分页查询
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pageNo", (pageNo - 1) * 10);//起始位置
+		map.put("pageSize", 10);//每页10个
+		
+		model.addAttribute("orderList", adminOrderDao.orderInfoBypage(map));
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("pageNo", pageNo);
 		return "admin/orderManager";
 	}
 	
 	public String deleteorderManager(String orderId) {
 		adminOrderDao.deleteOrderBase(orderId);
-		return "forward:/adminOrder/orderInfo";
+		return "forward:/admin/orderInfo";
 	}
 
 }
