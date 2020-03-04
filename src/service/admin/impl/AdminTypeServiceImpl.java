0bef5369b4a2bab514097c92f702cfda12ee74f0
刘文东 <18041196499@163.com>
2020-03-04 17:30:15 +0800

    比对 AdminTypeServiceImpl.java

--- src/service/admin/impl/AdminTypeServiceImpl.java
+++ src/service/admin/impl/AdminTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package service.admin.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
 import dao.admin.AdminTypeDao;
+import po.GoodsType;
 import service.admin.AdminTypeService;
 import util.MyUtil;
 
@@ -20,15 +22,34 @@ public class AdminTypeServiceImpl implements AdminTypeService{
 	@Autowired
 	private AdminTypeDao adminTypeDao;
 
-	public String toAddType(Model model) {
-		model.addAttribute("allTypes", adminTypeDao.selectGoodsType());
+	public String toAddType(Model model,Integer pageNo) {
+		List<GoodsType> list = adminTypeDao.selectGoodsType();
+		int temp = list.size();
+		model.addAttribute("totalCount", temp);
+		int totalPage = 0;
+		if (temp == 0) {
+			totalPage = 0;//总页数
+		} else {
+			//返回大于或者等于指定表达式的最小整数
+			totalPage = (int) Math.ceil((double) temp / 10);
+		}
+		if (pageNo == null) {
+			pageNo = 1;
+		}
+		//分页查询
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pageNo", (pageNo - 1) * 10);//起始位置
+		map.put("pageSize", 8);//每页8个
+		model.addAttribute("allTypes", adminTypeDao.selectGoodsTypeByPage(map));
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("pageNo", pageNo);
 		return "admin/addType";
 	}
 
 	public String addType(String typeName, String code,Model model, HttpSession session) {
 		adminTypeDao.addType(MyUtil.getUUID(),typeName,code);
 		session.setAttribute("goodsType", adminTypeDao.selectGoodsType());
-		return "forward:/adminType/toAddType";
+		return "forward:/admin/toAddType";
 	}
 
 	public String toDeleteType(Model model) {
