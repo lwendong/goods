e7a6e729a7d6df12870d6792081c4aaaeb9220b7
刘文东 <18041196499@163.com>
2020-03-04 17:28:06 +0800

    比对 AdminNoticeServiceImpl.java

--- src/service/admin/impl/AdminNoticeServiceImpl.java
+++ src/service/admin/impl/AdminNoticeServiceImpl.java
@@ -1,5 +1,9 @@
 package service.admin.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +24,30 @@ public class AdminNoticeServiceImpl implements AdminNoticeService{
 	public String addNotice(Notice notice) {
 		notice.setId(MyUtil.getUUID());
 		adminNoticeDao.addNotice(notice);
-		return "forward:/adminNotice/deleteNoticeSelect";
+		return "forward:/admin/deleteNoticeSelect";
 	}
 	
-	public String deleteNoticeSelect(Model model) {
-		model.addAttribute("allNotices", adminNoticeDao.deleteNoticeSelect());
+	public String deleteNoticeSelect(Model model,Integer pageNo) {
+		List<Notice> list = adminNoticeDao.deleteNoticeSelect();
+		int temp = list.size();
+		model.addAttribute("totalCount", temp);
+		int totalPage = 0;
+		if (temp == 0) {
+			totalPage = 0;//总页数
+		} else {
+			//返回大于或者等于指定表达式的最小整数
+			totalPage = (int) Math.ceil((double) temp / 10);
+		}
+		if (pageNo == null) {
+			pageNo = 1;
+		}
+		//分页查询
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pageNo", (pageNo - 1) * 10);//起始位置
+		map.put("pageSize", 10);//每页10个
+		model.addAttribute("allNotices", adminNoticeDao.deleteNoticeSelectBypage(map));
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("pageNo", pageNo);
 		return "admin/deleteNoticeSelect";
 	}
 	
@@ -34,7 +57,7 @@ public class AdminNoticeServiceImpl implements AdminNoticeService{
 	}
 	public String deleteNotice(String noticeId) {
 		adminNoticeDao.deleteNotice(noticeId);
-		return "forward:/adminNotice/deleteNoticeSelect";
+		return "forward:/admin/deleteNoticeSelect";
 	}
 
 }
