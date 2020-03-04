430765e7b3bd55277025a90ea291a4c2045087e3
刘文东 <18041196499@163.com>
2020-03-04 17:31:24 +0800

    比对 AdminUserServiceImpl.java

--- src/service/admin/impl/AdminUserServiceImpl.java
+++ src/service/admin/impl/AdminUserServiceImpl.java
@@ -1,4 +1,8 @@
 package service.admin.impl;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +11,7 @@ import org.springframework.ui.Model;
 import dao.admin.AdminUserDao;
 import dao.before.CartDao;
 import dao.before.UserCenterDao;
+import po.User;
 import service.admin.AdminUserService;
 
 @Service("adminUserService")
@@ -14,27 +19,41 @@ import service.admin.AdminUserService;
 public class AdminUserServiceImpl implements AdminUserService{
 	@Autowired
 	private AdminUserDao adminUserDao;
-	@Autowired
-	private CartDao cartDao;
-	@Autowired
-	private UserCenterDao userCenterDao;
+
 	
-	public String userInfo(Model model) {
-		model.addAttribute("userList", adminUserDao.userInfo());
+	public String userInfo(Model model,Integer pageNo) {
+		List<User> list = adminUserDao.userInfo();
+		int temp = list.size();
+		model.addAttribute("totalCount", temp);
+		int totalPage = 0;
+		if (temp == 0) {
+			totalPage = 0;//总页数
+		} else {
+			//返回大于或者等于指定表达式的最小整数
+			totalPage = (int) Math.ceil((double) temp / 10);
+		}
+		if (pageNo == null) {
+			pageNo = 1;
+		}
+		//分页查询
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pageNo", (pageNo - 1) * 10);//起始位置
+		map.put("pageSize", 10);//每页10个
+		model.addAttribute("userList", adminUserDao.userInfoBypage(map));
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("pageNo", pageNo);
 		return "admin/userManager";
 	}
-	
-	public String deleteuserManager(String userId, Model model) {
-		//用户有关联
-		if(cartDao.selectCart(userId).size() > 0 ||
-				userCenterDao.myFocus(userId).size() > 0||
-				userCenterDao.myOrder(userId).size() > 0) {
-			model.addAttribute("msg", "用户有关联，不能删除！");
-			return "forward:/adminUser/userInfo";
-		}
-		if(adminUserDao.deleteuserManager(userId) > 0) 
-			model.addAttribute("msg", "成功删除用户！");
-		return "forward:/adminUser/userInfo";
+
+	public String upUserManager(String id, Model model) {
+		User user = adminUserDao.userOne(id);
+		int s = user.getState();
+		User user2 = new User();
+		user2.setState(user.getState());
+		user2.setId(id);
+		if(adminUserDao.upUserManager(user2) > 0) 
+			model.addAttribute("state", "用户状态已修改");
+			return "forward:/admin/userInfo";
 	}
 
 }
